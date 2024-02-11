6f2b7ea87b48911d2f06baf241dd3434217717d3
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-02-11 13:17:14 +0100

    last modific

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
-const creaTabellone = (celle) => {
-  const tabella = document.getElementById("creaTomb");
+const contenitore = document.getElementsByClassName("container")[0];
+//////////crea celle
+for (let i = 0; i < 70; i++) {
+  const celle = document.createElement("div");
+  celle.classList.add("celle");
+  celle.textContent = i + 1;
+  contenitore.appendChild(celle); //applica celle
+}
+//creo bottone
+const button = document.createElement("button");
+button.classList.add("btn");
+button.innerText = "Genera Numero";
+contenitore.appendChild(button);
+button.addEventListener("click", function () {
+  const nRandom = Math.floor(Math.random() * 60);
+  const celle = document.getElementsByClassName("celle");
+  //confronto numeri
+  for (let i = 0; i < celle.length; i++) {
+    if (parseInt(celle[i].textContent) === nRandom) {
+      celle[i].style.backgroundColor = "red";
+      break;
+    }
+  }
+});
+/////////////generazione cartelline
+const newSect = document.getElementsByClassName("centro")[0];
+const button1 = document.createElement("button");
+button1.innerText = "generate card";
+button1.classList.add("btn1");
+newSect.appendChild(button1);
+const section = document.getElementsByClassName("sezione")[0];
+button1.onclick = () => {
+  const cartelline = document.createElement("div");
+  cartelline.classList.add("cartelline");
 
-  for (let i = 0; i < 76; i++) {
-    const cellDiv = document.createElement("div");
-    const cellH3 = document.createElement("h3");
-    cellDiv.className = "cella";
-    cellH3.innerText = i + 1;
-    cellDiv.appendChild(cellH3);
-    tabella.appendChild(cellDiv);
-    console.log(cellDiv);
+  /* cartelline.appendChild(newSect); */
+  section.appendChild(cartelline);
+  for (let i = 0; i < 30; i++) {
+    const celle1 = document.createElement("div");
+    celle1.classList.add("celle1");
+    celle1.textContent = Math.floor(Math.random() * 70);
+    cartelline.appendChild(celle1);
   }
 };
-
-const btn1 = document.getElementById("go");
-btn1.onclick = function (e) {
-  const casualNum = Math.floor(Math.random() * 76);
-  const array = document.querySelectorAll(".cella");
-  console.log(casualNum, array[casualNum - 1]);
-};
-
-creaTabellone();
